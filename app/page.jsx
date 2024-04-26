f3276784b105d98a7c73df4679cfefb84cb0dc15
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2024-04-26 01:36:43 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="contenedor">
       <Image src={ImageMona} width={449} height={412} alt='imagen anime' className='w-[229px] h-[212px]' />
       <h1 className='mt-10 font-bold text-3xl mb-6'>cesarpgasuz.dev</h1>
-      <p>Todavia no termino mi pagina pero aqui puedes encontrar mis redes</p>
+      <p>Todavía no termino mi página pero aquí puedes encontrar mis redes</p>
       <ul className='leading-10'>
         <li><a href='https://www.instagram.com/cesarpgasuz_' target="_blank" rel="noopener noreferrer" className='underline'>Instagram</a></li>
         <li><a href='https://github.com/cesarpgasuz' target="_blank" rel="noopener noreferrer" className='underline'>Github</a></li>
